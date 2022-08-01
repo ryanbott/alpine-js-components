80e97e986deaba8cfd2d3af8429ab6bb9443d276
Ryan Bott <ryan@ryanbott.com>
2022-07-31 22:41:34 -0500

    postcss, tailwind install

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
